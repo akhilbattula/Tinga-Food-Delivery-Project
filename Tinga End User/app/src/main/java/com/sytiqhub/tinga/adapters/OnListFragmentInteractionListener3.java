4a8e2775bdef223b8e716c4c4ba8781992cd67ea
Akhil Battula <akhilbattula1997@gmail.com>
2019-08-02 11:47:42 +0530

    Deleting

--- Tinga End User/app/src/main/java/com/sytiqhub/tinga/adapters/OnListFragmentInteractionListener3.java
+++
@@ -1,9 +0,0 @@
-package com.sytiqhub.tinga.adapters;
-
-import com.sytiqhub.tinga.beans.OrderBean;
-
-public interface OnListFragmentInteractionListener3 {
-    // TODO: Update argument type and name
-    void onListFragmentInteraction(OrderBean item);
-
-}
